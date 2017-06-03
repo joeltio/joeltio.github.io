31024c028e3c3f2f9202b63e68c8854f8fa481eb
joeltio <joeltiojinhon@gmail.com>
2017-06-03 11:45:43 +0800

    Reorganised project and added css support

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var debug = process.env.NODE_ENV != 'production';
+const webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    devtool: debug ? 'inline-sourcemap' : false,
+    entry: './src/client.js',
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react', 'es2015'],
+                    plugins: [
+                        'react-html-attrs',
+                        'transform-class-properties',
+                        'transform-decorators-legacy',
+                        'react-css-modules',
+                    ],
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader?sourceMap',
+                    'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+                ]
+            }
+        ]
+    },
+    output: {
+        path: __dirname + '/build/',
+        filename: 'client.min.js'
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, soucemap: false })
+    ]
+};
