f4045adcb631fc07bd7106a2e6698100f7f3fead
joeltio <joeltiojinhon@gmail.com>
2017-06-03 12:12:44 +0800

    Added Sass support

--- webpack.config.js
+++ webpack.config.js
@@ -17,17 +17,36 @@ module.exports = {
                         'react-html-attrs',
                         'transform-class-properties',
                         'transform-decorators-legacy',
-                        'react-css-modules',
+                        [
+                            'react-css-modules',
+                            {
+                                'filetypes': {
+                                    ".scss": "postcss-scss"
+                                }
+                            }
+                        ]
+
                     ],
                 }
             },
             {
                 test: /\.css$/,
+                exclude: /node_modules/,
                 use: [
                     'style-loader?sourceMap',
                     'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader?sourceMap',
+                    'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+                    'sass-loader'
+                ]
             }
+
         ]
     },
     output: {
