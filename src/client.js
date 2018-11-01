da0af73355707228b7199771c7cdb5bed29b21ef
Joel Tio <joeltiojinhon@gmail.com>
2018-11-01 22:50:33 +0800

    Update client.js with routers and routes

--- src/client.js
+++ src/client.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, HashRouter } from "react-router-dom";
+
+import HomeLayout from "./home/index";
+import BlogLayout from "./blog/index";
 
 class Layout extends React.Component {
     render() {
         return (
-            <div class="container">
-                <h1>Hello!</h1>
-                <p>This site is a showcase of what I learnt from React.</p>
-                <p>It is currently empty as I still need to learn more.</p>
-                <p>Stay tuned!</p>
-            </div>
+            <HashRouter>
+                <div class="h-100 container">
+                    <Route path="/" exact component={HomeLayout} />
+                    <Route path="/blog/" component={BlogLayout} />
+                </div>
+            </HashRouter>
         );
     }
 }
