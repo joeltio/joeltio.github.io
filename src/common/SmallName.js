b8278f182f84f04a82a4727337b8e8bc0aa674ed
Joel Tio <joeltiojinhon@gmail.com>
2018-11-02 10:42:56 +0800

    Add inline option to SmallName

--- src/common/SmallName.js
+++ src/common/SmallName.js
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./smallName.css";
 
-const SmallName = props => (
-    <p className={styles.name}>{props.children}</p>
-);
+const SmallName = props => {
+    var nameStyles = styles.name;
+    if (props.inline) {
+        nameStyles += " " + styles.inline;
+    }
+    return (
+        <p className={nameStyles}>{props.children}</p>
+    );
+}
 
 export default SmallName;
