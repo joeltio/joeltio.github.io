89f207a4e5c8ef5eaec1a6ad0834e862b635edd2
Joel Tio <joeltiojinhon@gmail.com>
2018-11-02 16:44:58 +0800

    Update MainLayout to be functional

--- src/client.js
+++ src/client.js
@@ -5,18 +5,14 @@ import { Route, HashRouter } from "react-router-dom";
 import HomeLayout from "./home/index";
 import BlogLayout from "./blog/index";
 
-class Layout extends React.Component {
-    render() {
-        return (
-            <HashRouter>
-                <div class="h-100 container">
-                    <Route path="/" exact component={HomeLayout} />
-                    <Route path="/blog/" component={BlogLayout} />
-                </div>
-            </HashRouter>
-        );
-    }
-}
+const MainLayout = () => (
+    <HashRouter>
+        <div class="h-100 container">
+            <Route path="/" exact component={HomeLayout} />
+            <Route path="/blog/" component={BlogLayout} />
+        </div>
+    </HashRouter>
+);
 
 const app = document.getElementById("app");
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(<MainLayout/>, app);
