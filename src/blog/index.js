8e0e60ba1ecd8be45441e0bac2a90125cb072870
Joel Tio <joeltiojinhon@gmail.com>
2018-11-02 13:53:47 +0800

    Update blog index to receive posts from props

--- src/blog/index.js
+++ src/blog/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 import NavBar from "./NavBar";
+import BlogPost from "./BlogPost";
 
-const BlogLayout = () => (
-    <div>
-        <NavBar />
-    </div>
-);
+const BlogLayout = props => {
+    let posts = [];
+    for (let blogPost of props.posts) {
+        posts.push(
+            <BlogPost title={blogPost.title}
+                      datetime={blogPost.datetime}>
+                {blogPost.content}
+            </BlogPost>
+        );
+    }
+
+    return (
+        <div>
+            <NavBar />
+            <div class="mt-4">
+                {posts}
+            </div>
+        </div>
+    );
+};
 
 export default BlogLayout;
