4402da90125be72b0a01f0711d2b9a0750e76137
Joel Tio <joeltiojinhon@gmail.com>
2018-11-04 08:19:23 +0800

    Update blog index to use implicit return

--- src/blog/index.js
+++ src/blog/index.js
@@ -3,13 +3,11 @@ import React from "react";
 import NavBar from "./NavBar";
 import BlogPosts from "./BlogPosts";
 
-const BlogLayout = props => {
-    return (
-        <div>
-            <NavBar />
-            <BlogPosts />
-        </div>
-    );
-};
+const BlogLayout = () => (
+    <div>
+        <NavBar />
+        <BlogPosts />
+    </div>
+);
 
 export default BlogLayout;
