e88552d6fa5fc660c6a2e52a5d933a83846d9e7b
Joel Tio <joeltiojinhon@gmail.com>
2019-11-22 10:33:56 +0800

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -30,16 +30,28 @@ module.exports = {
                 test: /\.css$/,
                 exclude: /node_modules/,
                 use: [
-                    'style-loader?sourceMap',
-                    'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: 1
+                        }
+                    }
                 ]
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
                 use: [
-                    'style-loader?sourceMap',
-                    'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: 1
+                        }
+                    },
                     'sass-loader'
                 ]
             }
