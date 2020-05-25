c0cb55fa6fe8ea65ab422a34978098aa39b88b1d
Joel Tio <joeltiojinhon@gmail.com>
2020-05-25 22:39:08 +0800

    Add projects

--- src/about/index.js
+++ src/about/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import NavBar from "../common/NavBar";
 import AboutMe from "./AboutMe";
+import Projects from "./Projects";
 
 const AboutLayout = ({navLocations}) => (
     <div>
         <NavBar navLocations={navLocations} />
         <AboutMe />
+        <Projects />
     </div>
 )
 
