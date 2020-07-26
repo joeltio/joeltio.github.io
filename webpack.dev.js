19871c00b69518783b0385a870df136df288a4e2
Joel Tio <joeltiojinhon@gmail.com>
2020-07-26 10:48:28 +0800

    Add webpack server to dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.template.html',
+            filename: '../index.html',
+        }),
+    ]
 });
