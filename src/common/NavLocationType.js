b42ddd6d9b80cd2501a2c43683a68e64e62fd597
Joel Tio <joeltiojinhon@gmail.com>
2020-07-26 10:56:55 +0800

    Delete old files and replace with gatsby starter blog

--- src/common/NavLocationType.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-const NavLocationType = PropTypes.shape({
-    name: PropTypes.string,
-    path: PropTypes.string,
-    component: PropTypes.elementType,
-});
-
-export default NavLocationType;
